0fdf6c1a77af825df711ff719f006ba07516e236
Maksym-Kartawcew <maxkartavtsev@gmail.com>
2024-01-07 20:22:24 +0100

    searchBar

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,22 @@
-import  { Hero }  from '../components/index';
+import { Hero, SearchBar, CustomFilter } from "../components/index";
 
 export default function Home() {
   return (
     <main className="overflow-hidden">
       <Hero />
+      <div className="mt-12 padding-x padding-y max-w" id="discover">
+        <div className="home__text-container">
+          <h1 className="text-4xl font-extrabold">Car Catalogue</h1>
+          <p>Explore the cars You might like</p>
+
+          <div className="home__filters">
+            <SearchBar />
+            <div className="home__filter-container"></div>
+            <CustomFilter title="fuel" />
+            <CustomFilter title="year" />
+          </div>
+        </div>
+      </div>
     </main>
-  )
+  );
 }
--- types/index.ts
+++ types/index.ts
@@ -5,7 +5,12 @@ export interface CustomButtonProps {
   btnType?: "button" | "submit";
   containerStyles?: string;
   title: string;
-//   textStyles?: string;
-//   rightIcon?: string;
+  //   textStyles?: string;
+  //   rightIcon?: string;
   handleClick?: MouseEventHandler<HTMLButtonElement>;
+}
+
+export interface SearchManufacturerProps {
+  manufacturer: string;
+  setManufacturer: (manufacturer: string) => void;
 }
