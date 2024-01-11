2852d95395d3c5aad879a8707c8e2675521633df
Maksym-Kartawcew <maxkartavtsev@gmail.com>
2024-01-11 16:15:38 +0100

    SearchParams - new pathname

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,12 +1,45 @@
 "use client";
-import { useState } from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import { SearchButton, SearchManufacturer } from "./index";
+import { useRouter } from "next/navigation";
 
 const SearchBar = () => {
   const [manufacturer, setManufacturer] = useState("");
   const [model, setModel] = useState("");
-  const handleSearch = () => {};
+  const router = useRouter();
+
+  const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault;
+
+    if (manufacturer === "" && model === "") {
+      return alert("Please fill in the searchbar");
+    }
+
+    updateSearchParams(model.toLowerCase(), manufacturer.toLowerCase());
+  };
+
+  const updateSearchParams = (model: string, manufacturer: string) => {
+    const searchParams = new URLSearchParams(window.location.search);
+
+    if (model) {
+      searchParams.set("model", model);
+    } else {
+      searchParams.delete("model");
+    }
+
+    if (manufacturer) {
+      searchParams.set("manufacturer", manufacturer);
+    } else {
+      searchParams.delete("manufacturer");
+    }
+
+    const newPathname = `${
+      window.location.pathname
+    }? ${searchParams.toString()}`;
+
+    router.push(newPathname);
+  };
 
   return (
     <form className="searchbar" onSubmit={handleSearch}>
